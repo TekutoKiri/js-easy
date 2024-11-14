fb9ec805aaaa56993c257c76b3a2bc2632a782c7
TekutoKiri <kn23-d.soloviov@nubip.edu.ua>
2024-11-14 13:43:05 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,22 +1,16 @@
-// Задача: Написати функцію, яка приймає рядок і повертає його у зворотному порядку,
-//  при цьому пропускаючи всі цифри.
-
 function reverseWithoutNumbers(str) {
-	// Масив для збереження символів, які не є цифрами
 	const chars = [];
 	
 	for (let char of str) {
-	  // Перевіряємо, чи символ не є цифрою
 	  if (!(char >= '0' && char <= '9')) {
 		chars.push(char);
 	  }
 	}
 	
-	// Зворотній порядок для масиву з символами і повернення рядка
 	return chars.reverse().join('');
-  }
+}
 
-console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
-console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
+console.log(reverseWithoutNumbers("hello123world456"));
+console.log(reverseWithoutNumbers("abc123xyz"));
 
 module.exports = reverseWithoutNumbers;
