68f29273522d11cb913d307e580c9323c8b3ac71
TekutoKiri <kn23-d.soloviov@nubip.edu.ua>
2024-11-14 13:42:30 +0200

    Update task5.js

--- tasks/task5.js
+++ tasks/task5.js
@@ -1,22 +1,16 @@
-// Задача: Напишіть функцію countOccurrences, яка приймає рядок і символ,
-//  і повертає кількість входжень цього символу в рядку.
-
-
 function countOccurrences(str, char) {
-	let count = 0; // Ініціалізуємо лічильник
+	let count = 0;
   
 	for (let i = 0; i < str.length; i++) {
-	  // Якщо поточний символ дорівнює char, збільшуємо лічильник
 	  if (str[i] === char) {
 		count++;
 	  }
 	}
 	
-	return count; // Повертаємо кількість входжень
-  }
+	return count;
+}
 
-// Приклади використання:
-console.log(countOccurrences("hello world", 'o')); // Виведе: 2
-console.log(countOccurrences("banana", 'a'));       // Виведе: 3
+console.log(countOccurrences("hello world", 'o'));
+console.log(countOccurrences("banana", 'a'));
 
 module.exports = countOccurrences;
